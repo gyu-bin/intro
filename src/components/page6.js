baa54b448ce553e7591adaec1b3171018cd9a2dd
gyu-bin <rbqls6651@gmail.com>
2021-04-19 10:10:21 +0900

    .

--- src/components/page6.js
+++ src/components/page6.js
@@ -1,40 +1,29 @@
 import React, { Component } from 'react';
-import { ButtonBack, ButtonNext } from 'pure-react-carousel';
-import Typed from 'typed.js';
+import { ButtonBack, Dot } from 'pure-react-carousel';
 
+// 연락처
 class Page6 extends Component {
-  componentDidMount() {
-    const options = {
-      strings: [
-        "Contact"
-      ],
-      typeSpeed: 150,
-      startDelay: 500,
-      backSpeed: 100,
-      backDelay: 1300,
-      loop: true,
-    }
-    this.typed = new Typed(this.el, options);
-  }
-  // 메모리 누수 방지
-  componentWillUnmount() {
-    this.typed.destroy();
-  }
   render() {
     return (
-      <div className="page-2">
+      <div className="page-6">
         <ButtonBack className="to-top">
           <span className="fa fa-angle-double-up" aria-hidden="true"></span>
         </ButtonBack>
         <div className="container-fluid">
-          <div className="page-2-content">
-            <h4 className="page-2-h4">이곳은  &nbsp;
-            <strong id="typed" style={{ whiteSpace: "pre" }} ref={(el) => { this.el = el; }}></strong>페이지입니다.<strong>연락주세요</strong></h4>
+          <div className="page-6-content">
+            <h1 className="h1-p6">더 알아보기</h1>
+            <p><span className="fa fa-envelope" aria-hidden="true"></span> rbqls6651@naver.com </p>
+            <p className="more-contact"> 이메일로 연락주시면 상세한 이력서 보내드리겠습니다.<br /> 당신과 함께 성장하고 싶습니다.</p>
+            {/* <p><a className="btn btn-dark btn-sm" href="https://drive.google.com/file/d/1QKHYW7chyCMqBYJicGGsJihjcLueDlIi/view?usp=sharing" role="button" target="_blank">
+              <span className=" fa fa-file-text" aria-hidden="true"></span>
+              &nbsp; Résumé</a></p> */}
           </div>
         </div>
-        <ButtonNext className="to-bottom">
-          <span className="fa fa-angle-double-down" aria-hidden="true"></span>
-        </ButtonNext>
+        <Dot slide={0} className="to-first">
+          <span className="touch-guide">처음 페이지로</span>
+          <br />
+          <span className="fa fa-step-backward" aria-hidden="true"></span>
+        </Dot>
       </div>
     );
   }
